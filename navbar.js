069468c6df3b57f277840aa7928cdea2001100df
nitro207 <128526025+nitro207@users.noreply.github.com>
2024-02-20 17:40:35 -0800

    created navbar

---
+++ navbar.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Fetch and insert the content of the navbar.html into the "navbarContainer" div
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("navbarContainer").innerHTML = html;
+        });
+});
